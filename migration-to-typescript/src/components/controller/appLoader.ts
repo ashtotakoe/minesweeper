82e95faa32a96fc292a1a94a12a12a62effcc837
Егор Мартынов <egormartynov12@gmail.com>
2023-06-06 19:05:26 +0300

    chore: set up prettier

--- migration-to-typescript/src/components/controller/appLoader.ts
+++ migration-to-typescript/src/components/controller/appLoader.ts
@@ -1,11 +1,11 @@
-import Loader from './loader';
+import Loader from './loader'
 
 class AppLoader extends Loader {
-    constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
-        });
-    }
+  constructor() {
+    super('https://newsapi.org/v2/', {
+      apiKey: 'e468a836826b42c29b5d1f74a624da3c', // получите свой ключ https://newsapi.org/
+    })
+  }
 }
 
-export default AppLoader;
+export default AppLoader
--- migration-to-typescript/src/components/view/appView.ts
+++ migration-to-typescript/src/components/view/appView.ts
@@ -1,21 +1,21 @@
-import News from './news/news';
-import Sources from './sources/sources';
+import News from './news/news'
+import Sources from './sources/sources'
 
 export class AppView {
-    constructor() {
-        this.news = new News();
-        this.sources = new Sources();
-    }
+  constructor() {
+    this.news = new News()
+    this.sources = new Sources()
+  }
 
-    drawNews(data) {
-        const values = data?.articles ? data?.articles : [];
-        this.news.draw(values);
-    }
+  drawNews(data) {
+    const values = data?.articles ? data?.articles : []
+    this.news.draw(values)
+  }
 
-    drawSources(data) {
-        const values = data?.sources ? data?.sources : [];
-        this.sources.draw(values);
-    }
+  drawSources(data) {
+    const values = data?.sources ? data?.sources : []
+    this.sources.draw(values)
+  }
 }
 
-export default AppView;
+export default AppView
