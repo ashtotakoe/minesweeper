add79cb67b024388481d20591a231d3fea2ea106
Egor <egormartynov12@gmail.com>
2023-07-02 20:01:59 +0300

    feat: add game passed popup

--- rss-css-selectors/src/app/components/game-passed-popup/game-passed-popup.ts
+++ rss-css-selectors/src/app/components/game-passed-popup/game-passed-popup.ts
@@ -1,11 +1,19 @@
 import { BaseComponent } from '../../utils/base-component'
 
 export class GamePassedPopup extends BaseComponent {
+  private wrapper = new BaseComponent({
+    parent: this.element,
+    attribute: {
+      textContent: 'congrads! you passed the game',
+      className: 'game-passed-popup-wrapper__popup',
+    },
+  })
+
   constructor(parent: HTMLElement) {
     super({
       parent,
       attribute: {
-        className: 'game-passed-popup',
+        className: 'game-passed-popup-wrapper',
       },
     })
   }
